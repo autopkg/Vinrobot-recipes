bf685cb2555a0035c332135caa3008d58254220b
Vinrobot <vinrobot97@gmail.com>
2019-07-21 22:00:15 +0200

    Add SharedProcessors

---
+++ SharedProcessor/DebugPrint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from autopkglib import Processor, ProcessorError
+
+__all__ = [ "DebugPrint" ]
+
+class DebugPrint(Processor):
+	"""Print a variable in console. Useful to debug."""
+	description = __doc__
+
+	input_variables = {
+		"variable_name": {
+			"required": True,
+			"description": "The name of the variable to print."
+		}
+	}
+
+	output_variables = {
+	}
+
+	def main(self):
+		variable_name = self.env['variable_name']
+		if variable_name == "*":
+			print self.env
+		else:
+			print self.env[variable_name]
+
+
+if __name__ == "__main__":
+	PROCESSOR = DebugPrint()
+	PROCESSOR.execute_shell()
